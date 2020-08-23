1f49b328937006c59eefd14bd966b54db6d66056
trywahyu <m.trywahyuar@gmail.com>
2020-08-24 03:54:52 +0800

    --- Auto-Git Commit ---

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -5,7 +5,8 @@
     <!-- Page Header -->
     <div class="page-header row no-gutters py-4">
       <div class="col-12 col-sm-6 text-center text-sm-left mb-4 mb-sm-0">
-        <span class="text-uppercase page-subtitle">Dashboard</span>
+        <span class="text-uppercase page-subtitle">Informasi
+        </span>
         <h3 class="page-title">Info Merk Kendaraan</h3>
       </div>
       <div class="col-12 col-sm-6 d-flex align-items-center">
@@ -73,9 +74,9 @@
 <!-- Transaction History Table -->
 <div class="row">
   <div class="col">
-    <div class="card card-small mb-4">
+    <div class="card card-small mb-2">
       <div class="card-header border-bottom">
-        <h6 class="m-0">Active Users</h6>
+        
       <div class="card-body p-0 pb-3 text-center">
         <table class="table mb-0">
           <thead class="bg-light">
