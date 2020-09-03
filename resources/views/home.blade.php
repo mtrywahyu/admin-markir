49a303320e0788c50f996c1d3cabdbc4e1028d13
muhammad try wahyu ar <m.trywahyuar@gmail.com>
2020-09-04 01:53:46 +0800

    --- Auto-Git Commit ---

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -80,20 +80,18 @@
     </div>
   </div>
   <!-- End Small Stats Blocks -->
-  <div class="row">
+
+  {{-- Chart balok parkir Terkini --}}
+<div class="row">
     <div class="col col-lg-8 col-md-12 col-sm-12 mb-4">
       <div class="card card-small h-100">
         <div class="card-header border-bottom">
-          <h6 class="m-0">Sessions</h6>
+          <h6 class="m-0">Data Per-Hari</h6>
           <div class="block-handle"></div>
         </div>
         <div class="card-body pt-0">
           <div class="row border-bottom py-2 bg-light">
             <div class="col-12 col-sm-6 d-flex mb-2 mb-sm-0">
-              <div class="btn-group btn-group-sm btn-group-toggle d-flex my-auto mx-auto mx-sm-0" data-toggle="buttons">
-                <label class="btn btn-white active">
-                  <input type="radio" name="options" id="option1" autocomplete="off" checked=""> Data Per-Hari </label>
-              </div>
             </div>
           </div>
           <div id="analytics-overview-sessions-legend"></div>
@@ -101,8 +99,26 @@
         </div>
       </div>
     </div>
-  </div><script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    {{-- End Chart Parkir TerKini --}}
 
+
+  <div class="col-lg-4 col-md-6 col-sm-6 mb-4">
+    <!-- Chart Pie Jukirs -->
+    <div class="card ubd-stats card-small h-100">
+      <div class="card-header border-bottom">
+        <h6 class="m-0">Jukir</h6>
+        <div class="block-handle"></div>
+      </div>
+      <div class="card-body d-flex flex-column">
+        <canvas id="my"></canvas>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <!-- end Chart Pie Jukir -->
+  </div>
+</div>
+{{-- Chart parkir terkini --}}
 <script>
   var label = [];
   @foreach($a as $item)
@@ -122,18 +138,62 @@
       data: {
           labels: label,
           datasets: [{
-              label: 'Data Parkir Terkini ',
+              label: 'Jenis Kendaraan',
               backgroundColor: 'rgb(28, 173, 186)',
               borderColor: 'rgb(28, 173, 186)',
-              data: total
+              data: total,
           }]
+          
       },
   
       // Configuration options go here
-      options: {}
+      options: { 
+        
+        }
   });
  </script>
- 
+ {{-- END chart Parkir Terkini --}}
+
+ <canvas id="myChart2"width="400" height="400"></canvas>
+<script>
+var ctx = document.getElementById('myChart2').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+</script>
   
   
 @endsection
