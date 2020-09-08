fa5b9eabc99336482842e336a5869066b906b9e5
muhammad try wahyu ar <m.trywahyuar@gmail.com>
2020-09-09 01:41:29 +0800

    --- Auto-Git Commit ---

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,6 +19,7 @@ class HomeController extends Controller
         $ref_jenis_kendaraan = refbiaya::all();
         $a=array();
         $b=array();
+        $c=array();
         // return $parkir->first()->UserKendaraan->RefJenisKendaraan2;
         foreach($ref_jenis_kendaraan as $item){
             array_push($a,$item->jenis_kendaraan);
@@ -30,6 +31,18 @@ class HomeController extends Controller
             }
             array_push($b, $i);
         }
+
+        foreach($jukir as $item){
+            array_push($a,$item->jukir );
+            $i = 0;
+            foreach($jukir as $items){
+                if($items->UserKendaraan->RefJenisKendaraan2->id_ref_kendaraan == $item->id_ref_kendaraan){
+                    $i++;
+                }
+            }
+            array_push($b, $i);
+        }
+
         // return $b;
         // return $a;
     return view("home",compact('jukir','terkini','riwayat','user_akun','a','b'));
