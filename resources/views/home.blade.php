ef3d1722883bf3da07444d51702f6704e23c7e6d
muhammad try wahyu ar <m.trywahyuar@gmail.com>
2020-09-26 04:46:53 +0800

    --- Auto-Git Commit ---

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section("content")
 
  <!-- / .main-navbar -->
- <div class="main-content-container container-fluid px-4">
+  <div class="main-content-container container-fluid px-4">
     <!-- Page Header -->
     <div class="page-header row no-gutters py-4">
       <div class="col-12 col-sm-4 text-center text-sm-left mb-4 mb-sm-0">
@@ -11,192 +11,193 @@
     </div>
     <!-- End Page Header -->
 
- <div class="row">
-    <div class="col-12 col-md-6 col-lg-3 mb-4">
-      <div class="stats-small card card-small">
-        <div class="card-body px-0 pb-0">
-          <div class="d-flex px-3">
-            <div class="stats-small__data">
-              <span class="stats-small__label mb-1 text-uppercase">
-                <span class="stats-medium__label mb-1 text-uppercase">Parkir Terkini</span>
-              </span>
-              <a href="/parkirmasuk">
-                <img src="{{asset('images/mobil-masuk.png')}} ">
-              <p>Total:{{$terkini}}</p>
-            </a>
+      <div class="row">
+          <div class="col-12 col-md-6 col-lg-3 mb-4">
+            <div class="stats-small card card-small">
+              <div class="card-body px-0 pb-0">
+                <div class="d-flex px-3">
+                  <div class="stats-small__data">
+                    <span class="stats-small__label mb-1 text-uppercase">
+                      <span class="stats-medium__label mb-1 text-uppercase">Parkir Terkini</span>
+                    </span>
+                    <a href="/parkirmasuk">
+                      <img src="{{asset('images/mobil-masuk.png')}} ">
+                    <p>Total:{{$terkini}}</p>
+                  </a>
+                  </div>
+                </div>
+                <canvas height="60" class="analytics-overview-stats-small-1"></canvas>
+              </div>
             </div>
           </div>
-          <canvas height="60" class="analytics-overview-stats-small-1"></canvas>
-        </div>
-      </div>
-    </div>
-    <div class="col-12 col-md-6 col-lg-3 mb-4">
-      <div class="stats-small card card-small">
-        <div class="card-body px-0 pb-0">
-          <div class="d-flex px-3">
-            <div class="stats-small__data">
-              <span class="stats-small__label mb-1 text-uppercase">Riwayat</span>
-              <a href="/parkirkeluar">
-              <img src="{{asset('images/mobil-keluar.png')}} ">
-              <p>Total:{{$riwayat}}</p>
-            </a>
-            </div> 
-          </div>
-          <canvas height="60" class="analytics-overview-stats-small-2"></canvas>
-        </div>
-      </div>
-    </div>
-    <div class="col-12 col-md-6 col-lg-3 mb-4">
-      <div class="stats-small card card-small">
-        <div class="card-body px-0 pb-0">
-          <div class="d-flex px-3">
-            <div class="stats-small__data">
-              <span class="stats-small__label mb-1 text-uppercase">Jukir</span>
-              <a href="/jukir">
-                <img src="{{asset('images/avatar.png')}} ">
-                <p>Total:{{$jukir}}</p>
-              </a>
+          <div class="col-12 col-md-6 col-lg-3 mb-4">
+            <div class="stats-small card card-small">
+              <div class="card-body px-0 pb-0">
+                <div class="d-flex px-3">
+                  <div class="stats-small__data">
+                    <span class="stats-small__label mb-1 text-uppercase">Riwayat</span>
+                    <a href="/parkirkeluar">
+                    <img src="{{asset('images/mobil-keluar.png')}} ">
+                    <p>Total:{{$riwayat}}</p>
+                  </a>
+                  </div> 
+                </div>
+                <canvas height="60" class="analytics-overview-stats-small-2"></canvas>
+              </div>
             </div>
           </div>
-          <canvas height="60" class="analytics-overview-stats-small-3"></canvas>
-        </div>
-      </div>
-    </div>
-    <div class="col-12 col-md-6 col-lg-3 mb-4">
-      <div class="stats-small card card-small">
-        <div class="card-body px-0 pb-0">
-          <div class="d-flex px-3">
-            <div class="stats-small__data">
-              <span class="stats-small__label mb-1 text-uppercase">User</span>
-              <a href="/userbiodata">
-                <img src="{{asset('images/user.png')}} ">
-              <p>Total:{{$user_akun}}</p>
-              </a>
+          <div class="col-12 col-md-6 col-lg-3 mb-4">
+            <div class="stats-small card card-small">
+              <div class="card-body px-0 pb-0">
+                <div class="d-flex px-3">
+                  <div class="stats-small__data">
+                    <span class="stats-small__label mb-1 text-uppercase">Jukir</span>
+                    <a href="/jukir">
+                      <img src="{{asset('images/avatar.png')}} ">
+                      <p>Total:{{$jukir}}</p>
+                    </a>
+                  </div>
+                </div>
+                <canvas height="60" class="analytics-overview-stats-small-3"></canvas>
+              </div>
             </div>
           </div>
-          <canvas height="60" class="analytics-overview-stats-small-4"></canvas>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- End Small Stats Blocks -->
-
-  {{-- Chart balok parkir Terkini --}}
-<div class="row">
-    <div class="col col-lg-8 col-md-12 col-sm-12 mb-4">
-      <div class="card card-small h-100">
-        <div class="card-header border-bottom">
-          <h6 class="m-0">Data Per-Hari</h6>
-          <div class="block-handle"></div>
-        </div>
-        <div class="card-body pt-0">
-          <div class="row border-bottom py-2 bg-light">
-            <div class="col-12 col-sm-6 d-flex mb-2 mb-sm-0">
+          <div class="col-12 col-md-6 col-lg-3 mb-4">
+            <div class="stats-small card card-small">
+              <div class="card-body px-0 pb-0">
+                <div class="d-flex px-3">
+                  <div class="stats-small__data">
+                    <span class="stats-small__label mb-1 text-uppercase">User</span>
+                    <a href="/userbiodata">
+                      <img src="{{asset('images/user.png')}} ">
+                    <p>Total:{{$user_akun}}</p>
+                    </a>
+                  </div>
+                </div>
+                <canvas height="60" class="analytics-overview-stats-small-4"></canvas>
+              </div>
             </div>
           </div>
-          <div id="analytics-overview-sessions-legend"></div>
-          <canvas id="myChart"></canvas>
         </div>
-      </div>
-    </div>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    {{-- End Chart Parkir TerKini --}}
+        <!-- End Small Stats Blocks -->
 
-
-  <div class="col-lg-4 col-md-6 col-sm-6 mb-4">
-    <!-- Chart Pie Jukirs -->
-    <div class="card ubd-stats card-small h-100">
-      <div class="card-header border-bottom">
-        <h6 class="m-0">Jukir</h6>
-        <div class="block-handle"></div>
-      </div>
-      <div class="card-body d-flex flex-column">
-        <canvas id="myChart2" width="100" class="analytics-users-by-device mt-3 mb-4"></canvas>
-        <div class="ubd-stats__legend w-75 m-auto pb-4">
-        </div>
-      </div>
-      <div class="card-footer border-top">
-        <div class="row">
-          <div class="col">
-            <p>Silahkan Cek Data Juru Parkir Sebelum Memverifikasi Juru Parkir</p>
+        {{-- Chart balok parkir Terkini --}}
+      <div class="row">
+          <div class="col col-lg-8 col-md-12 col-sm-12 mb-4">
+            <div class="card card-small h-100">
+              <div class="card-header border-bottom">
+                <h6 class="m-0">Data Per-Hari</h6>
+                <div class="block-handle"></div>
+              </div>
+              <div class="card-body pt-0">
+                <div class="row border-bottom py-2 bg-light">
+                  <div class="col-12 col-sm-6 d-flex mb-2 mb-sm-0">
+                  </div>
+                </div>
+                <div id="analytics-overview-sessions-legend"></div>
+                <canvas id="myChart"></canvas>
+              </div>
+            </div>
           </div>
-          <div class="col text-right view-report">
-            <a href="/jukir">View full report &rarr;</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    <!-- end Chart Pie Jukir -->
-  </div>
-</div>
-{{-- Chart parkir terkini --}}
-<script>
-  var label = [];
-  @foreach($a as $item)
-    label.push('<?php echo $item; ?>');
-  @endforeach
-  var total = [];
-  @foreach($b as $item)
-    total.push('<?php echo $item; ?>');
-  @endforeach
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+          {{-- End Chart Parkir TerKini --}}
 
-  
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
-      // The type of chart we want to create
-      type: 'bar',
-    
-      // The data for our dataset
-      data: {
-          labels: label,
-          datasets: [{
-              label: 'Jenis Kendaraan',
-              backgroundColor: 'rgb(28, 173, 186)',
-              borderColor: 'rgb(28, 173, 186)',
-              data: total,
-          }]
-          
-      },
-  
-      // Configuration options go here
-      options: { 
-        
-        }
-  });
- </script>
- {{-- END chart Parkir Terkini --}}
 
-<script>
- var ctx = document.getElementById('myChart2').getContext('2d');
- var chart = new Chart(ctx, {
-     // The type of chart we want to create
-     type: 'doughnut',
- 
-     // The data for our dataset
-     data: {
-         labels: ['Ter-Verifikasi', 'Belum Ter-Verifikasi'],
-         datasets: [{
-             label: 'juru parkir',
-             data: [{{$jukir2}}, {{$jukir1}} ],
-             backgroundColor: [
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-            ],
-             
-            borderColor: [
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 99, 132, 1)'
+              <div class="col-lg-4 col-md-6 col-sm-6 mb-4">
+                <!-- Chart Pie Jukirs -->
+                <div class="card ubd-stats card-small h-100">
+                  <div class="card-header border-bottom">
+                    <h6 class="m-0">Jukir</h6>
+                    <div class="block-handle"></div>
+                  </div>
+                  <div class="card-body d-flex flex-column">
+                    <canvas id="myChart2" width="100" class="analytics-users-by-device mt-3 mb-4"></canvas>
+                    <div class="ubd-stats__legend w-75 m-auto pb-4">
+                    </div>
+                  </div>
+                  <div class="card-footer border-top">
+                    <div class="row">
+                      <div class="col">
+                        <p>Silahkan Cek Data Juru Parkir Sebelum Memverifikasi Juru Parkir</p>
+                      </div>
+                      <div class="col text-right view-report">
+                        <a href="/jukir">View full report &rarr;</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+                <!-- end Chart Pie Jukir -->
+              </div>
+            </div>
+            {{-- Chart parkir terkini --}}
+            <script>
+              var label = [];
+              @foreach($a as $item)
+                label.push('<?php echo $item; ?>');
+              @endforeach
+              var total = [];
+              @foreach($b as $item)
+                total.push('<?php echo $item; ?>');
+              @endforeach
+
+              
+                var ctx = document.getElementById('myChart').getContext('2d');
+                var chart = new Chart(ctx, {
+                  // The type of chart we want to create
+                  type: 'bar',
                 
-                
-            ]
-             
-         }]
-     },
- 
-     // Configuration options go here
-     options: {}
- })
-</script>  
+                  // The data for our dataset
+                  data: {
+                      labels: label,
+                      datasets: [{
+                          label: 'Jenis Kendaraan',
+                          backgroundColor: 'rgb(28, 173, 186)',
+                          borderColor: 'rgb(28, 173, 186)',
+                          data: total,
+                      }]
+                      
+                  },
+              
+                  // Configuration options go here
+                  options: { 
+                    
+                    }
+              });
+            </script>
+            {{-- END chart Parkir Terkini --}}
+
+            <script>
+            var ctx = document.getElementById('myChart2').getContext('2d');
+            var chart = new Chart(ctx, {
+                // The type of chart we want to create
+                type: 'doughnut',
+            
+                // The data for our dataset
+                data: {
+                    labels: ['Ter-Verifikasi', 'Belum Ter-Verifikasi'],
+                    datasets: [{
+                        label: 'juru parkir',
+                        data: [{{$jukir2}}, {{$jukir1}} ],
+                        backgroundColor: [
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 99, 132, 0.2)',
+                        ],
+                        
+                        borderColor: [
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 99, 132, 1)'
+                            
+                            
+                        ]
+                        
+                    }]
+                },
+            
+                // Configuration options go here
+                options: {}
+            })
+            </script>  
+ </div>
   
 @endsection
--- resources/views/master/layouts.blade.php
+++ resources/views/master/layouts.blade.php
@@ -18,6 +18,81 @@
 
   <body class="h-100">
     
+      <div class="container-fluid icon-sidebar-nav">
+        <div class="row">
+          <!-- Main Sidebar -->
+          <aside class="main-sidebar col-12 col-md-3 col-lg-2 px-0">
+            <div class="main-navbar">
+              <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
+                <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
+                  <div class="d-table m-auto">
+                    <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="images/shards-dashboards-logo.svg" alt="Shards Dashboard">
+                  </div>
+                </a>
+                <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">
+                  <i class="material-icons">&#xE5C4;</i>
+                </a>
+              </nav>
+            </div>
+            <form action="#" class="main-sidebar__search w-100 border-right d-sm-flex d-md-none d-lg-none">
+              <div class="input-group input-group-seamless ml-3">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">
+                    <i class="fas fa-search"></i>
+                  </div>
+                </div>
+                <input class="navbar-search form-control" type="text" placeholder="Search for something..." aria-label="Search">
+              </div>
+            </form>
+            <div class="nav-wrapper">
+              <h6 class="main-sidebar__nav-title">Dashboards</h6>
+              <ul class="nav nav--no-borders flex-column">
+                <li class="nav-item">
+                  <a class="nav-link " href="/home">
+                    <i class="material-icons">home</i>
+                    <span>Beranda</span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link " href="/parkirmasuk">
+                    <i class="material-icons">moped</i>
+                    <span>Parkir Terkini</span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link " href="/parkirkeluar">
+                    <i class="material-icons">history</i>
+                    <span>Riwayat Parkir</span>
+                  </a>
+                </li>
+              </ul>
+              <h6 class="main-sidebar__nav-title">Templates</h6>
+              <ul class="nav nav--no-borders flex-column">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">
+                    <i class="material-icons">&#xE7FD;</i>
+                    <span>Master Data</span>
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-small">
+                    <a class="dropdown-item " href="/userbiodata">Data User</a>
+                    <a class="dropdown-item " href="/jukir">Data Juru Parkir</a>
+                    <a class="dropdown-item " href="/info">Merk Kendaraan</a>
+                    <a class="dropdown-item " href="/refbiaya">Biaya Kendaraan</a>
+                    
+                  </div>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle " data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                    <i class="material-icons">&#xE2C7;</i>
+                    <span>Laporan</span>
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-small">
+                    <a class="dropdown-item " href="file-manager-list.html">Laporan Parkir</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </aside>
   <div class="container-fluid">
       <div class="row">
         <main class="main-content col-lg-12 col-md-12 col-sm-12 p-0">
@@ -25,12 +100,7 @@
             <div class="container p-0">
               <!-- Main Navbar -->
               <nav class="navbar align-items-stretch navbar-light flex-md-nowrap p-0">
-                <a class="navbar-brand" href="#" style="line-height: 25px;">
-                  <div class="d-table m-auto">
-                    <img id="main-logo" class="d-inline-block align-top mr-1 ml-3" style="max-width: 25px;" src="{{ asset('images/shards-dashboards-logo.svg') }}" alt="Shards Dashboard">
-                    <span class="d-none d-md-inline ml-1">Markir</span>
-                  </div>
-                </a>
+               
                 <ul class="navbar-nav border-left flex-row border-right ml-auto">
                   <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-nowrap px-3" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
@@ -53,32 +123,12 @@
                 </nav>
               </nav>
             </div> <!-- / .container -->
-          </div> <!-- / .main-navbar -->
-          <div class="header-navbar collapse d-lg-flex p-0 bg-white border-top">
+      <!-- / .main-navbar -->
+           <div class="header-navbar collapse d-lg-flex p-0 bg-white border-top"> 
+          
             <div class="container">
               <div class="row">
                 <div class="col">
-                  <ul class="nav nav-tabs border-0 flex-column flex-lg-row">
-                    <li class="nav-item">
-                      <a href="/home" class="nav-link" ><i class="material-icons">home</i> Beranda</a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="/parkirmasuk" class="nav-link"><i class="material-icons">moped</i> Parkir Terkini</a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="/parkirkeluar" class="nav-link"><i class="material-icons">history</i> Riwayat Parkir</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link" data-toggle="dropdown"><i class="material-icons">&#xE2C7;</i>Master Data</a>
-                      <div class="dropdown-menu dropdown-menu-small">
-                        <a href="/userbiodata" class="dropdown-item">Users</a>
-                        <a href="/jukir" class="dropdown-item">Jukir</a>
-                        <a href="/info" class="dropdown-item">Merk Kendaraan</a>
-                        <a href="/refbiaya" class="dropdown-item">Info Biaya Kendaraan</a>
-                      </div>
-                      
-                    </li>
-                  </ul>
                 </div>
               </div>
             </div>
